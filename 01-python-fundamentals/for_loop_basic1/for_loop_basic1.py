98d04b932eb0420c217081aadaf4d88e42d522aa
unknown <khalilmeddeb123@gmail.com>
2023-07-12 14:56:56 +0100

    loop-basic1

---
+++ 01-python-fundamentals/for_loop_basic1/for_loop_basic1.py
@@ -0,0 +1,24 @@
+for i in range(0, 150):
+    print(i)
+
+
+print(list(range(5,1000,5)))
+
+
+for i in range(100):
+    if (i % 5 == 0 ):
+        print("Coding")
+        i % 10 == 0
+        print("Coding Dojo")
+
+
+sum = []
+for i in range(1, 500000+1):
+        sum = sum + i
+
+print("The final sum") 
+
+
+for i in range(2018,0,-4):
+    i +=1
+    print ("i")
